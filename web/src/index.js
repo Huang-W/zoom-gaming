9dc15e437b4e05cabb62b6d4d449b0cbe405720f
Ward Huang <ward.huang@sjsu.edu>
2021-04-23 20:42:36 -0700

    updated html/js for integration

--- web/src/index.js
+++ web/src/index.js
@@ -8,39 +8,39 @@ import { InputMap } from "./input";
 const SERVER_ADDR = "127.0.0.1";
 
 (function() {
-  let input_dc = null;
-  let webSocket = new WebSocket(`ws://${SERVER_ADDR}:8080/demo`);
+  let peerConnection = null; // webrtc connection
+  let input_dc = null; // keyboard events are sent to the server using this
+  let webSocket = new WebSocket(`ws://${SERVER_ADDR}:8080/demo`); // session description is sent/received via websocket
   webSocket.binaryType = "arraybuffer" // blob or arraybuffer
 
-  webSocket.addEventListener("open", event => {
-    console.log("ws open");
-  })
-  webSocket.addEventListener("message", event => {
-    handleWebsocketEvent(event);
-  });
-  webSocket.addEventListener("close", event => {
-    console.log("ws closing");
-  });
-  webSocket.onerror = function(event) {
-    console.error("WebSocket error observed:", event);
-  };
-
-  let peerConnection = null;
-
-  let showError = (error) => {
-    console.log(error);
-    const errorNode = document.querySelector('#error');
-    if (errorNode.firstChild) {
-      errorNode.removeChild(errorNode.firstChild);
+  let handleWebsocketEvent = (event) => {
+    if ( Object.getPrototypeOf(event) === MessageEvent.prototype ) {
+      let remote_sdp_answer = new pb.SessionDescription.deserializeBinary(event.data);
+      if (remote_sdp_answer) {
+        console.log("received a remote answer from server...");
+        peerConnection.setRemoteDescription({
+          type: "answer",
+          sdp: remote_sdp_answer.getSdp()
+        });
+      } else {
+        console.error("not an sdp");
+      }
+    } else {
+      console.log(`Received event with prototype of ${Object.getPrototypeOf(event)}`);
     }
-    errorNode.appendChild(document.createTextNode(error.message || error));
   }
+
+  webSocket.addEventListener("open", event => { console.log("ws open"); });
+  webSocket.addEventListener("message", event => { handleWebsocketEvent(event); });
+  webSocket.addEventListener("close", event => { console.log("ws closing"); });
+  webSocket.onerror = function(event) { console.error("WebSocket error observed:", event); };
 
   let startSession = (offer) => {
     let pb_sd = new pb.SessionDescription();
     pb_sd.setType(pb.SessionDescription.SDPType.SDP_TYPE_OFFER);
     pb_sd.setSdp(offer);
     let uint8_array = pb_sd.serializeBinary();
+    console.log("sending local offer to the server...");
     webSocket.send(uint8_array.buffer);
   }
 
@@ -58,7 +58,7 @@ const SERVER_ADDR = "127.0.0.1";
     });
   }
 
-  let startRemoteSession = (remoteVideoNode, playerVideoNodes, stream) => {
+  let startRemoteSession = (remoteVideoNode) => {
     let pc;
 
     return Promise.resolve().then(() => {
@@ -69,10 +69,6 @@ const SERVER_ADDR = "127.0.0.1";
       // game streaming
       pc.addTransceiver('audio', {'direction': 'recvonly'});
       pc.addTransceiver('video', {'direction': 'recvonly'});
-
-      // camera + mic
-      // pc.addTransceiver('audio', {'direction': 'sendonly'});
-      // pc.addTransceiver('video', {'direction': 'sendonly'});
 
       pc.ontrack = ({ track, streams }) => {
         console.info('ontrack triggered');
@@ -117,77 +113,21 @@ const SERVER_ADDR = "127.0.0.1";
         }
       });
 
-      stream && stream.getTracks().forEach(track => {
-        pc.addTrack(track, stream);
-      });
       return createOffer(pc);
     }).then(offer => {
-      // console.info(offer);
       startSession(offer);
     }).then(() => pc);
   }
 
   document.addEventListener('DOMContentLoaded', () => {
-    let selectedScreen = 0;
-    const playerVideos = document.querySelector('#player-videos');
     const remoteVideo = document.querySelector('#remote-video');
-    const screenSelect = document.querySelector('#screen-select');
-    const startStop = document.querySelector('#start-stop');
 
-    const option = document.createElement('option');
-    option.appendChild(document.createTextNode('Screen 1'));
-    option.setAttribute('value', 0);
-    screenSelect.appendChild(option);
-
-    screenSelect.addEventListener('change', evt => {
-      selectedScreen = parseInt(evt.currentTarget.value, 10);
-    });
-
-    const enableStartStop = (enabled) => {
-      if (enabled) {
-        startStop.removeAttribute('disabled');
-      } else {
-        startStop.setAttribute('disabled', '');
-      }
+    if (!peerConnection) {
+      startRemoteSession(remoteVideo).then(pc => {
+        remoteVideo.style.setProperty('visibility', 'visible');
+        peerConnection = pc;
+      }).catch((error) => { console.error(error); });
     }
-
-    const setStartStopTitle = (title) => {
-      startStop.removeChild(startStop.firstChild);
-      startStop.appendChild(document.createTextNode(title));
-    }
-
-    startStop.addEventListener('click', () => {
-      enableStartStop(false);
-
-      const videoConstraints = {
-          height: { ideal: 240, max: 300 },
-          width: { ideal: 320, max: 400 }
-      };
-      const userMediaPromise = Promise.resolve(null);
-      // const userMediaPromise = navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }) || Promise.resolve(null);
-      if (!peerConnection) {
-        userMediaPromise.then(stream => {
-          let el = document.createElement("video");
-          el.srcObject = stream;
-          el.muted = true;
-          el.autoplay = true;
-          playerVideos.appendChild(el);
-          return startRemoteSession(remoteVideo, playerVideos, stream).then(pc => {
-            remoteVideo.style.setProperty('visibility', 'visible');
-            peerConnection = pc;
-          }).catch(showError).then(() => {
-            enableStartStop(true);
-            setStartStopTitle('Stop');
-          });
-        })
-      } else {
-        peerConnection.close();
-        peerConnection = null;
-        enableStartStop(true);
-        setStartStopTitle('Start');
-        remoteVideo.style.setProperty('visibility', 'collapse');
-      }
-    });
   });
 
   window.addEventListener('beforeunload', () => {
@@ -198,30 +138,4 @@ const SERVER_ADDR = "127.0.0.1";
       webSocket.close();
     }
   });
-
-  let handleAnswer = (sdp) => {
-    if (sdp instanceof pb.SessionDescription) {
-      // console.info(sdp.getSdp());
-      peerConnection.setRemoteDescription({
-        type: "answer",
-        sdp: sdp.getSdp()
-      });
-    }
-  }
-
-  let handleWebsocketEvent = (event) => {
-    if ( Object.getPrototypeOf(event) === MessageEvent.prototype ) {
-      let remote_sdp_answer = new pb.SessionDescription.deserializeBinary(event.data);
-      if (remote_sdp_answer) {
-        console.log("sdp!");
-        handleAnswer(remote_sdp_answer);
-      } else {
-        console.error("not an sdp");
-      }
-    } else if ( Object.getPrototypeOf(event) === CloseEvent.prototype ) {
-      console.log("ws closing");
-    } else {
-      console.log(`Received event with prototype of ${Object.getPrototypeOf(event)}`);
-    }
-  }
 })()
