a330431432a785fe2f0f1ae8efe5c260d3d5e0ec
aditya.agrawal1 <aditya.agrawal@sjsu.edu>
2021-05-07 07:02:34 -0700

    final

--- client/src/routes/TabooGame/src/utils/fbConfig.js
+++ client/src/routes/TabooGame/src/utils/fbConfig.js
@@ -4,13 +4,13 @@ import "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_DB_URL,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: JSON.stringify(process.env.REACT_APP_DB_URL),
+  authDomain: JSON.stringify(process.env.REACT_APP_AUTH_DOMAIN),
+  projectId: JSON.stringify(process.env.REACT_APP_PROJECT_ID),
+  storageBucket: JSON.stringify(process.env.REACT_APP_STORAGE_BUCKET),
+  messagingSenderId: JSON.stringify(process.env.REACT_APP_MSG_SENDER_ID),
+  appId: JSON.stringify(process.env.REACT_APP_APP_ID),
+  measurementId: JSON.stringify(process.env.REACT_APP_MEASUREMENT_ID)
 };
 
 // Initialize Firebase
